f8eb91a9f92e9c02a685ffef9c3178675b10b7f8
Jan Gołda <jan.golda@gmail.com>
2019-05-06 01:27:11 +0200

    Added port argument to exchange rates server

--- lab4-middleware/exchange-rates/index.js
+++ lab4-middleware/exchange-rates/index.js
@@ -6,15 +6,25 @@ const CHANGES_INTERVAL = 5000;
 const CHANGES_CHANCE = 0.3;
 const CHANGES_MAX = 0.2;
 const DEFAULT_HOST = '0.0.0.0';
-const DEFAULT_PORT = '50001';
+
+// parse arguments
+const argv = require('yargs')
+    .option('p', {
+        alias: 'port',
+        type: 'number',
+        describe: 'Port to listen on',
+        demandOption: true
+    })
+    .help()
+    .argv;
 
 // create server
 const ExchangeRatesServer = require('./exchange-rates-server')
 const server = new ExchangeRatesServer();
 
 // bind server
-server.bind(DEFAULT_HOST + ':' + DEFAULT_PORT, grpc.ServerCredentials.createInsecure());
-logger.info(`Server has been bound to ${DEFAULT_HOST}:${DEFAULT_PORT}`);
+server.bind(DEFAULT_HOST + ':' + argv['port'], grpc.ServerCredentials.createInsecure());
+logger.info(`Server has been bound to ${DEFAULT_HOST}:${argv['port']}`);
 
 // start server
 server.start();
